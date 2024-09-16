e0136740cb77f1a00229487e8583039470d543a4
abra-abe <abewills17@gmail.com>
2024-09-16 17:07:22 +0300

    added route parameters...

--- server.py
+++ server.py
@@ -9,3 +9,20 @@ def index():
 @app.get("/name")
 def names():
     return {"name": "Abraham"}
+
+# using route parameters
+# creating a dictionary to be fetched
+users = {
+    1: {
+        "name": "Abraham",
+        "age": 24,
+    },
+    2: {
+        "name": "Greg",
+        "age": 24
+    }
+}
+
+@app.get("/users/{user_id}")
+def get_users(user_id: int):
+    return users[user_id]
