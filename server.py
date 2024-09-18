42b782d95ad256eccd84b190ab38247e99f10352
abra-abe <abewills17@gmail.com>
2024-09-18 14:03:10 +0300

    - combined route/path parameters with query parameters - used post method

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Path
-from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -11,36 +11,62 @@ def index():
 def names():
     return {"name": "Abraham"}
 
-# using route parameters
+
 # creating a dictionary to be used
 users = {
     1: {
         "name": "Abraham",
         "age": 24,
+        "occupation": "developer"
     },
     2: {
         "name": "Greg",
-        "age": 24
+        "age": 24,
+        "occupation": "Defensive security"
     },
     3: {
         "name": "Sam",
-        "age": 24
+        "age": 24,
+        "occupation": "offensive security"
     }
 }
 
+# user class
+class User(BaseModel):
+    name: str
+    age: int
+    occupation: str
+
+# Route parameters
 @app.get("/user/{user_id}")
 def get_users(user_id: int = Path(description="Enter the user ID")):
-    return users[user_id]
-
-@app.get("/users/{user_id}")
-def get_users(user_id: Optional[int] = None):
     return users[user_id]
 
 
 # query parameters
 @app.get("/search-user")
-def search_users(user_name: str):
+def search_users(user_name: str = ""):
     for user_id in users:
         if users[user_id]["name"] == user_name:
             return users[user_id]
         return {"message": "Data not found"}
+    
+
+# Combining route and query parameters
+@app.get("/user_details/{user_id}")
+def user_details(user_id: int, user_age: int = 24):
+    # user_occupation = users[user_id]["age"]
+    if users[user_id]["age"] == user_age:
+        return {"occupation": users[user_id]["occupation"]}
+    return {"message": "Data not found"}
+
+
+# Post Methods
+
+@app.post("/add-user")
+def create_user(user_id: int, user: User):
+    if user_id in users:
+        return {"error": "User with id: "+str(user_id)+" already exists"}
+    
+    users[user_id] = user
+    return users
